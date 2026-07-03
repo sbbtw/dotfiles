29abc1cac99c6e158c449c546d840b817a0ec86b
Suraj Biswas <surajb0027@gmail.com>
2026-07-04 02:47:22 +0530

    modified i3

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,4 +3,7 @@ if status is-interactive
 # Commands to run in interactive sessions can go here
 end
 alias vi=nvim
-alias ls='eza --icons=always --color=always --group-directories-first'
+function copy_from_history
+  history | fzf | xclip -selection clipboard
+end
+bind \ct copy_from_history
